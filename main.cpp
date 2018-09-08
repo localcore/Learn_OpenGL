6c20ffc809010c0a8fe8413e0fdfe85723f24c9c
umbertolo <umberto.loria@gmail.com>
2018-09-08 12:47:03 +0200

    Dealing with shaders!

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 #include <cmath>
+#include <fstream>
+#include <sstream>
+
+struct ShaderProgramSource {
+	std::string vertexSource;
+	std::string fragmentSource;
+};
 
 void processInput (GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -13,29 +20,73 @@ void framebuffer_size_callback (GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
+static ShaderProgramSource parseShader (const std::string& filepath) {
+	enum class ShaderType {
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::ifstream stream(filepath);
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line)) {
+		if (line.find("#shader") != std::string::npos) {
+			if (line.find("vertex") != std::string::npos) {
+				type = ShaderType::VERTEX;
+			} else if (line.find("fragment") != std::string::npos) {
+				type = ShaderType::FRAGMENT;
+			}
+		} else {
+			ss[(int) type] << line << '\n';
+		}
+	}
+	return {ss[0].str(), ss[1].str()};
+}
+
+static unsigned int compileShader (unsigned int type, const std::string& source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char message[length];
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment");
+		std::cout << " shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+	return id;
+}
+
+static unsigned int createShader (const std::string& vertexSha, const std::string& fragmentSha) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexSha);
+	unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentSha);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+
+GLFWwindow* createWindow ();
+
 int main () {
 
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window;
-	window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
-
-	if (!window) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
+	GLFWwindow* window = createWindow();
 
 	float vertices[] = {
 			-0.5f, -0.5f,
@@ -50,7 +101,12 @@ int main () {
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	ShaderProgramSource source = parseShader("res/shaders/triangle.glsl");
+
+
+	unsigned int shader = createShader(source.vertexSource, source.fragmentSource);
+	glUseProgram(shader);
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -66,6 +122,32 @@ int main () {
 		glfwPollEvents();
 	}
 
+	//glDeleteProgram(shader);
+
 	glfwTerminate();
 	return 0;
+}
+
+
+GLFWwindow* createWindow () {
+
+	if (!glfwInit()) {
+		return nullptr;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
+
+	if (!window) {
+		glfwTerminate();
+		return nullptr;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return nullptr;
+	}
+	return window;
 }
